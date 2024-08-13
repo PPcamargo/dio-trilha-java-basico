c4b767a3d383b2bbc746e4e08a8ea447681508ae
PPcamargo <ppcamargo82@gmail.com>
2024-08-13 19:22:56 -0300

    sistema-tv

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,34 +1,20 @@
 
 public class Operadores {
     public static void main(String[] args) throws Exception {
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a  numeroDois " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois " + simNao);
+
+        simNao = numero1 < numero2;
         
-    //    String nomeCompleto = "LINGUAGEM " + "JAVA";
-    //    System.out.println(nomeCompleto);
-
-    String concatenacao = "?";
-
-    concatenacao = 1+1+1+"1";
-
-    System.out.println(concatenacao);
-
-    concatenacao = 1+"1"+1+1;
-
-    System.out.println(concatenacao);
-
-    concatenacao = 1+"1"+1+"1";
-
-    System.out.println(concatenacao);
-
-    concatenacao = "1"+1+1+1;
-    
-    System.out.println(concatenacao);
-
-    concatenacao = "1"+(1+1+1);
-
-    System.out.println(concatenacao);
-
+        System.out.println("numeroUm é maior que numeroDois " + simNao);
     }
-    
-
-
 }
