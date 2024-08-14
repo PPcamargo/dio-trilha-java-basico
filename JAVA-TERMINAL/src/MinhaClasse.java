3b1c84a3db84b5b44f78d4948eb54188945fb56d
PPcamargo <ppcamargo82@gmail.com>
2024-08-14 18:49:29 -0300

    caixa-eletronico

--- JAVA-TERMINAL/src/MinhaClasse.java
+++ JAVA-TERMINAL/src/MinhaClasse.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class MinhaClasse {
     public static void main(String[] args){
         System.out.println("Oi Turma, fiu executado pelo Terminal");
