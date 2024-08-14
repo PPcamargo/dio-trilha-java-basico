eb54b0832a29fcce007b97738f6bba664a99872e
PPcamargo <ppcamargo82@gmail.com>
2024-08-14 19:15:35 -0300

    Atualizacao-resultado-escolar

--- controle-de-fluxo/src/CaixaEletronico.java
+++ controle-de-fluxo/src/CaixaEletronico.java
@@ -3,9 +3,12 @@ public class CaixaEletronico {
         double saldo = 25.0;
         double valorSolicitado = 26.0;
 
-        if(valorSolicitado < saldo)
+        if(valorSolicitado < saldo){
         saldo = saldo - valorSolicitado;
+        System.out.println("Novo saldo " + saldo);
+        
+        }else
+        System.out.println("Saldo insufciente");
 
-        System.out.println(saldo);
     }
 }
